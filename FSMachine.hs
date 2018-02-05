6444ad66c73e526e8e44bcf0a3254347817bf3b6
Nikola Valesova <bihanojko@localhost.localdomain>
2018-02-05 22:52:49 +0100

    Arguments and file parsing done, beginning of k-indistinguishability

---
+++ FSMachine.hs
@@ -0,0 +1,21 @@
+module FSMachine where
+
+type TInput = String
+type TState = String
+type TSymbol = Char
+
+
+data Transition = Trans
+    { fromState :: TState
+    , withSymbol :: TSymbol
+    , toState :: TState
+    } deriving (Eq)
+
+
+data FSMachine = FSM
+    { states :: [TState]
+    -- , alphabet :: [TSymbol]
+    , transitions :: [Transition]
+    , startState :: TState
+    , endStates :: String
+    } deriving (Eq)
--- xvales02.hs
+++ xvales02.hs
@@ -6,53 +6,97 @@ import System.Exit
 import System.Directory
 import Data.List.Split
 
+import FSMachine
+
 
 main :: IO ()
 main = do
     args <- getArgs
     let (process, filename) = parseArgs args
     inputContent <- getInput filename
-    let (parsedContent) = parseContent inputContent
+    let (fSMachine) = parseContent (lines inputContent)
 
-    print process
-    print filename
-    print inputContent
-    print parsedContent
+    -- print process
+    -- print filename
+    -- print inputContent
+    -- printFSMachine fSMachine
+
+    print (computeKIndistinguishability fSMachine 2)
+
+    -- if process then minimalizeFSM fSMachine
+    -- else printFSMachine fSMachine
 
     return ()
 
 
+
+printFSMachine :: FSMachine -> IO ()
+printFSMachine fsm = do
+    putStrLn (concat (intersperse "," (states fsm)))
+    -- putStrLn (alphabet fsm)
+    putStrLn (startState fsm)
+    putStrLn (endStates fsm)
+    mapM_ printTransition (transitions fsm)
+
+
+
+printTransition :: Transition -> IO ()
+printTransition transition = do
+    putStrLn $ fromState transition ++ "," ++ [(withSymbol transition)] ++ "," ++ (toState transition)   
+
+
+
 parseArgs :: [String] -> (Bool, String)
-parseArgs args
-    | param == "-i" = (False, filename)
-    | param == "-t" = (True, filename)
+parseArgs [] = error "Error in input arguments. Usage: ./dka-2-mka [-i|-t] [filename]"
+parseArgs [x]
+    | x == "-i" = (False, "None")
+    | x == "-t" = (True, "None")
     | otherwise = error "Error in input arguments. Usage: ./dka-2-mka [-i|-t] [filename]"
-    where   param = if length args == 1 || length args == 2
-                    then args !! 0
-                    else ""
-            filename =  if length args == 2
-                        then args !! 1
-                        else "None"
+parseArgs [x, y]
+    | x == "-i" = (False, y)
+    | x == "-t" = (True, y)
+    | otherwise = error "Error in input arguments. Usage: ./dka-2-mka [-i|-t] [filename]"
+parseArgs _ = error "Error in input arguments. Usage: ./dka-2-mka [-i|-t] [filename]"
+
 
 
 getInput :: String -> IO String
 getInput filename
     | filename == "None" = do
-        content <- getContents
+        content <- hGetContents stdin
         return content
-        -- return (lines content)
     | otherwise = do
         correctFilename <- doesFileExist filename
         if correctFilename
             then do 
                 content <- readFile filename
                 return content
-                -- return (lines content)
             else error "The input file does not exist!"
 
 
-parseContent :: String -> [[Char]]
-parseContent content = do
-    -- ct <- lines content
-    ctt <- splitOn "," content
-    return ctt
+
+parseContent :: [String] -> FSMachine
+parseContent (states:startState:finalStates:transitions) =
+    if null transitions
+        then error "no transitions"
+        else FSM getStates (map getRule transitions) startState finalStates
+        -- else FSM getStates getAlph (map getRule transitions) startState finalStates
+    where
+        getStates = splitOn "," states
+        -- getAlph = "we do not really care for alphabet"
+        getRule rule = getRule2 (splitOn "," rule)
+        getRule2 :: [String] -> Transition
+        getRule2 [q1, [sym], q2] =
+            Trans q1 sym q2
+        getRule2 _ = error "bad transition syntax"
+parseContent _ = error "bad syntax"
+
+
+
+-- minimalizeFSM :: FSMachine -> Int
+-- minimalizeFSM fSMachine =
+--     computeKIndistinguishability 0 1
+
+
+computeKIndistinguishability :: FSMachine -> Int -> Int
+computeKIndistinguishability fSMachine k = k
