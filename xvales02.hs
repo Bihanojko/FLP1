7760c31859f51891903196dba8d13933b3d44b90
Nikola Valesova <bihanojko@localhost.localdomain>
2018-02-08 20:01:10 +0100

    FSM completion done

--- xvales02.hs
+++ xvales02.hs
@@ -29,9 +29,9 @@ main = do
 printFSMachine :: FSMachine -> IO ()
 printFSMachine fsm = do
     putStrLn (concat (intersperse "," (states fsm)))
-    putStrLn (alphabet fsm)
+    -- putStrLn (alphabet fsm)
     putStrLn (startState fsm)
-    putStrLn (concat (intersperse "," (endStates fsm)))    
+    putStrLn (concat (intersperse "," (endStates fsm)))   
     mapM_ printTransition (transitions fsm)
 
 
@@ -74,44 +74,73 @@ parseContent (states : startState : finalStates : transitions) =
     if null transitions
         then error "no transitions"
         else FSM getStates (getAlph transitions) (map getRule transitions) startState getFinalStates
-        -- else FSM getStates (map getRule transitions) (read startState :: Int) getFinalStates
     where
         getStates = splitOn "," states
-        -- getStates = map (read :: String -> Int) (splitOn "," states)
+        getFinalStates = splitOn "," finalStates
         getAlph transitions = nub (map getSymbol transitions)
         getSymbol transition = head (splitOn "," transition !! 1)
         getRule rule = getRule2 (splitOn "," rule)
-        getRule2 :: [String] -> Transition
         getRule2 [q1, [sym], q2] =
-        -- Trans (read q1 :: Int) sym (read q2 :: Int)
             Trans q1 sym q2
         getRule2 _ = error "bad transition syntax"
-        getFinalStates = splitOn "," finalStates
-        -- getFinalStates = map (read :: String -> Int) (splitOn "," finalStates)
 parseContent _ = error "bad syntax"
 
 
-
+-- TODO
 minimalizeFSM :: FSMachine -> IO ()
 minimalizeFSM fSMachine = do
-    let prevIndistinguishability = compute0Indistinguishability (states fSMachine) (endStates fSMachine)
-    let nextIndistinguishability = computeKIndistinguishability (transitions fSMachine) prevIndistinguishability
+    let completeFSM = getCompleteFSM fSMachine
+    let prevIndistinguishability = compute0Indistinguishability (states completeFSM) (endStates completeFSM)
+    let nextIndistinguishability = computeKIndistinguishability (transitions completeFSM) prevIndistinguishability
     print prevIndistinguishability
     print nextIndistinguishability
 
 
+getCompleteFSM :: FSMachine -> FSMachine
+getCompleteFSM fSMachine = do
+    if (isFSMComplete fSMachine) then fSMachine
+    else completeFSM fSMachine
+
+
+isFSMComplete :: FSMachine -> Bool
+isFSMComplete fSMachine =
+    if length (transitions fSMachine) == (allTransitionsCount fSMachine) then True
+    else False
+    where 
+        allTransitionsCount fSMachine = length (states fSMachine) * length (alphabet fSMachine)
+
+
+completeFSM :: FSMachine -> FSMachine
+completeFSM fSMachine = do
+    let missingTransitions = getMissingTransitions fSMachine
+    let sinkState = getSink (states fSMachine)
+    let newTransitions = [Trans x y sinkState | (x, y) <- missingTransitions]
+    updateFSM fSMachine (sinkState : states fSMachine) ((transitions fSMachine) ++ newTransitions)
+    where
+        updateFSM x allStates allTrans = x {states = sort allStates, transitions = allTrans}
+
+
+getSink :: [TState] -> TState
+getSink states = do
+    let statesInt = [read x :: Int | x <- states]
+    show (head ([0..1000] \\ statesInt)) :: TState
+
+
+getMissingTransitions :: FSMachine -> [(TState, TSymbol)]
+getMissingTransitions fSMachine = do
+    let allTransitions = [(state, symbol) | state <- states fSMachine, symbol <- alphabet fSMachine]
+    let definedTransitions = [(fromState x, withSymbol x) | x <- transitions fSMachine]    
+    allTransitions \\ definedTransitions
+    
 
 compute0Indistinguishability :: [TState] -> [TState] -> [[TState]]
 compute0Indistinguishability states endStates = endStates : [states \\ endStates]
 
 
-
+-- TODO
 computeKIndistinguishability :: [Transition] -> [[TState]] -> [[TState]]
-computeKIndistinguishability transitions prevInd = do
-    let x = map todo prevInd
-    x
+computeKIndistinguishability transitions prevInd =
+    -- let xxx = [todo x | x <- prevInd]
+    prevInd
 
-todo :: [TState] -> [TState]
-todo listStates = listStates
-
--- TODO zuplnit KA pred minimalizaci, check vstup, zda je validni
+-- TODO remove unused functions and imports
