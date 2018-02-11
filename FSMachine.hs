9d7ef6026e52b0ba774c71159be41b3415b991c8
Nikola Valesova <bihanojko@localhost.localdomain>
2018-02-11 17:00:14 +0100

    Added header and comments

--- FSMachine.hs
+++ FSMachine.hs
@@ -1,21 +1,30 @@
+-- dka-2-mka
+-- Funkcionalni a logicke programovani
+-- Nikola Valesova, xvales02
+
 module FSMachine where
 
-type TInput = String
+-- data type of states
 type TState = String
+-- data type of symbols
 type TSymbol = Char
 
 
+-- data type of transitions
+-- delta(q, a) = p -> fromState = q, withSymbol = a, toState = p
 data Transition = Trans
-    { fromState :: TState
-    , withSymbol :: TSymbol
-    , toState :: TState
+    { fromState :: TState,
+      withSymbol :: TSymbol,
+      toState :: TState
     } deriving (Eq)
 
 
+-- data type of a FSM
+-- (Q, Sigma, delta, q, F) == (states, alphabet, transitions, startState, endStates)
 data FSMachine = FSM
-    { states :: [TState]
-    , alphabet :: [TSymbol]
-    , transitions :: [Transition]
-    , startState :: TState
-    , endStates :: [TState]
+    { states :: [TState],
+      alphabet :: [TSymbol],
+      transitions :: [Transition],
+      startState :: TState,
+      endStates :: [TState]
     } deriving (Eq)
