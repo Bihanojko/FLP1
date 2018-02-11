89a1da4ed955571da0b7618c9538cd1ae67ff4d9
Nikola Valesova <bihanojko@localhost.localdomain>
2018-02-11 15:11:54 +0100

    Added newline abstaction testing

--- dka-2-mka-test.py
+++ dka-2-mka-test.py
@@ -26,23 +26,38 @@ for idx, inputFile in enumerate(sorted(InputFiles)):
     intro = '\tTEST ' + str(idx + 1) + ': '
     reference_output_path = './Tests/Input/' + inputFile
 
-    if filecmp.cmp(OutputFilepath + '-i/' + inputFile, reference_output_path, False):
-        sys.stdout.write(intro + "\033[0;32m" + "SUCCESS!\n" + "\033[0;0m")
-    else:
-        sys.stdout.write(intro + "\033[1;31m" + "FAIL!" + "\033[0;0m" + " (" + inputFile + ")\n")
-        failures += 1
-
+    with open(OutputFilepath + '-i/' + inputFile, 'r') as o:
+        with open(reference_output_path, 'r') as ro:
+            failed = False
+            oContent = '\n'.join([x for x in o.read().split("\n") if x.strip()!=''])
+            roContent = '\n'.join([x for x in ro.read().split("\n") if x.strip()!=''])
+            for lineO, lineRO in zip(oContent, roContent):
+                if lineO != lineRO and not (lineO + lineRO).isspace():
+                    failed = True
+            if failed:
+                sys.stdout.write(intro + "\033[1;31m" + "FAIL!" + "\033[0;0m" + " (" + inputFile + ")\n")
+                failures += 1
+            else: 
+                sys.stdout.write(intro + "\033[0;32m" + "SUCCESS!\n" + "\033[0;0m")
 
 sys.stdout.write("\nTESTING PARAMETER -t\n")
 for idx, inputFile in enumerate(sorted(InputFiles)):
     intro = '\tTEST ' + str(idx + 1) + ': '
     reference_output_path = './Tests/RefOutput/' + inputFile
 
-    if filecmp.cmp(OutputFilepath + '-t/' + inputFile, reference_output_path, False):
-        sys.stdout.write(intro + "\033[0;32m" + "SUCCESS!\n" + "\033[0;0m")
-    else:
-        sys.stdout.write(intro + "\033[1;31m" + "FAIL!" + "\033[0;0m" + " (" + inputFile + ")\n")
-        failures += 1
+    with open(OutputFilepath + '-t/' + inputFile, 'r') as o:
+        with open(reference_output_path, 'r') as ro:
+            failed = False            
+            oContent = '\n'.join([x for x in o.read().split("\n") if x.strip()!=''])
+            roContent = '\n'.join([x for x in ro.read().split("\n") if x.strip()!=''])
+            for lineO, lineRO in zip(oContent, roContent):
+                if lineO != lineRO and not (lineO + lineRO).isspace():
+                    failed = True
+            if failed:
+                sys.stdout.write(intro + "\033[1;31m" + "FAIL!" + "\033[0;0m" + " (" + inputFile + ")\n")
+                failures += 1
+            else: 
+                sys.stdout.write(intro + "\033[0;32m" + "SUCCESS!\n" + "\033[0;0m")
 
 
 print('\nAll tests checked, totalling ' + str(failures) + ' errors!')
